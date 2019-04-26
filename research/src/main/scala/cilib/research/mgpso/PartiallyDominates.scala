f180213b6a8ab736c1e273b828d2b8fc603c0e4f
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-04-26 14:32:11 +0200

    Added PartiallyDominated.scala

---
+++ research/src/main/scala/cilib/research/mgpso/PartiallyDominates.scala
@@ -0,0 +1,49 @@
+package cilib.research.mgpso
+import cilib.research.core.Benchmark
+import scalaz._
+import Scalaz._
+
+object PartiallyDominates {
+	def apply(envX: Benchmark)(particleA: MGParticle, particleB: MGParticle, random_indices: NonEmptyList[Int]): Boolean =
+		if (particleA.pos.fitness == particleB.pos.fitness){
+			false
+		} else {
+			var check = false
+			if (envX.controlParameters.swarmSizes.size == 3){
+
+        val partialParticleA = List[Double](particleA.pos.fitness.toList(0), particleA.pos.fitness.toList(1), particleA.pos.fitness.toList(2))
+        val partialParticleB = List[Double](particleA.pos.fitness.toList(0), particleA.pos.fitness.toList(1), particleA.pos.fitness.toList(2))
+
+        partialParticleA.zip(partialParticleB).foldLeft(0)(
+					(a, c) => a +
+						{
+							if (envX.compareDoubles(c._1, c._2)) {
+								check = true
+								0
+							} else if (c._1 == c._2)
+								0
+							else
+								1
+						}
+				) == 0 && check
+
+			} else {
+
+				val partialParticleA = List[Double](particleA.pos.fitness.toList(random_indices.toList(0)), particleA.pos.fitness.toList(random_indices.toList(1)), particleA.pos.fitness.toList(random_indices.toList(2)))
+				val partialParticleB = List[Double](particleA.pos.fitness.toList(random_indices.toList(0)), particleA.pos.fitness.toList(random_indices.toList(1)), particleA.pos.fitness.toList(random_indices.toList(2)))
+
+				partialParticleA.zip(partialParticleB).foldLeft(0)(
+					(a, c) => a +
+						{
+							if (envX.compareDoubles(c._1, c._2)) {
+								check = true
+								0
+							} else if (c._1 == c._2)
+								0
+							else
+								1
+						}
+				) == 0 && check
+			}
+		}
+}
