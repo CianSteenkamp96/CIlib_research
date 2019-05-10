dc585111a8eed0f2e249f82e41e28f39e021886d
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-05-10 11:11:48 +0200

    Added extra parameters in order to save output files with a more descriptive name.

--- research/src/main/scala/cilib/research/core/ControlParameters.scala
+++ research/src/main/scala/cilib/research/core/ControlParameters.scala
@@ -11,16 +11,17 @@ case class ControlParameters(
     freqs: Option[(NonEmptyList[Int], (Int, Int, Int))] = None) // ##################################################### CHANGES ####################################################################
 {
 //  def setRandomIndices = this.copy(freqs = Some(this.freqs.get._1, (i123._1, i123._2, i123._3)))
-  def setRandomIndices_and_updateFreqs = // ##################################################### CHANGES ####################################################################
+  def setRandomIndices_and_updateFreqs
+    : ControlParameters = // ##################################################### CHANGES ####################################################################
     if (freqs.isDefined) {
       val probs: NonEmptyList[Double] = probFromFitness(fitnessFromFreq(freqs.get._1))
-      val randomIndices = get3Indices(probs)
-      this.copy(freqs = Some((
-        freqs.get._1.zipWithIndex.map(el =>
-          if ((el._2 == randomIndices._1) || (el._2 == randomIndices._2) || (el._2 == randomIndices._3))
-            el._1 + 1
-          else el._1),
-        randomIndices)))
+      val randomIndices: (Int, Int, Int) = get3Indices(probs)
+      this.copy(freqs = Some(
+        (freqs.get._1.zipWithIndex.map(el =>
+           if ((el._2 == randomIndices._1) || (el._2 == randomIndices._2) || (el._2 == randomIndices._3))
+             el._1 + 1
+           else el._1),
+         randomIndices)))
     } else
       this.copy()
 }
