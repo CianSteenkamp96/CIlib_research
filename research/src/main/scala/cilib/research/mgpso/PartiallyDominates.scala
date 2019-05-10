05662bc8720508be7f2f478eda1e4eb686946e38
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-05-10 11:10:05 +0200

    Added extra parameters in order to save output files with a more descriptive name.

--- research/src/main/scala/cilib/research/mgpso/PartiallyDominates.scala
+++ research/src/main/scala/cilib/research/mgpso/PartiallyDominates.scala
@@ -3,47 +3,28 @@ import cilib.research.core.Benchmark
 import scalaz._
 import Scalaz._
 
-object PartiallyDominates {
-	def apply(envX: Benchmark)(particleA: MGParticle, particleB: MGParticle, random_indices: (Int, Int, Int)): Boolean =
+object PartiallyDominates { // ################################## CHANGED - no longer check for 3 obj since the way the random indices are astored caters for all number of obj already ########################
+	def apply(envX: Benchmark)(particleA: MGParticle, particleB: MGParticle): Boolean =
 		if (particleA.pos.fitness == particleB.pos.fitness){
 			false
 		} else {
 			var check = false
-			if (envX.controlParameters.swarmSizes.size == 3){
 
-        val partialParticleA = List[Double](particleA.pos.fitness.toList(0), particleA.pos.fitness.toList(1), particleA.pos.fitness.toList(2))
-        val partialParticleB = List[Double](particleA.pos.fitness.toList(0), particleA.pos.fitness.toList(1), particleA.pos.fitness.toList(2))
+			// the 3 random indices readily available (even if only 3 obj)
+			val partialParticleA = List[Double](particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._1), particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._2), particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._3))
+			val partialParticleB = List[Double](particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._1), particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._2), particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._3))
 
-        partialParticleA.zip(partialParticleB).foldLeft(0)(
-					(a, c) => a +
-						{
-							if (envX.compareDoubles(c._1, c._2)) {
-								check = true
-								0
-							} else if (c._1 == c._2)
-								0
-							else
-								1
-						}
-				) == 0 && check
-
-			} else {
-
-				val partialParticleA = List[Double](particleA.pos.fitness.toList(random_indices._1), particleA.pos.fitness.toList(random_indices._2), particleA.pos.fitness.toList(random_indices._3))
-				val partialParticleB = List[Double](particleA.pos.fitness.toList(random_indices._1), particleA.pos.fitness.toList(random_indices._2), particleA.pos.fitness.toList(random_indices._3))
-
-				partialParticleA.zip(partialParticleB).foldLeft(0)(
-					(a, c) => a +
-						{
-							if (envX.compareDoubles(c._1, c._2)) {
-								check = true
-								0
-							} else if (c._1 == c._2)
-								0
-							else
-								1
-						}
-				) == 0 && check
-			}
+			partialParticleA.zip(partialParticleB).foldLeft(0)(
+				(a, c) => a +
+					{
+						if (envX.compareDoubles(c._1, c._2)) {
+							check = true
+							0
+						} else if (c._1 == c._2)
+							0
+						else
+							1
+					}
+			) == 0 && check
 		}
 }
