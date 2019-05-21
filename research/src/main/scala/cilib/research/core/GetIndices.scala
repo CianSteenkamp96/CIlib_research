f3a236b08d19276adbccfd5712f346fd60918b9c
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-05-21 15:40:56 +0200

    Added extra parameters in order to save output files with a more descriptive name.

--- research/src/main/scala/cilib/research/core/GetIndices.scala
+++ research/src/main/scala/cilib/research/core/GetIndices.scala
@@ -1,5 +1,5 @@
 // Cian Steenkamp
-// Still to be improved - remove vars !!!!!!!!!!!!!
+
 
 package cilib.research.core
 import scalaz._
@@ -29,21 +29,22 @@ object GetIndices {
   }
 
   // Returns 3 indices/objectives to be considered for diminance relation (partial-dominance)
+  // This function prioritizes the indices/objectives with the largest probabilities (least chosen objectives) even more...
+  // Since, only one pick is made to save time - previous versions made 3 picks and had a lot of repititions to ensure 3 unique indices
+  // This implementation makes one pick, gets the index, and then chooses the other 2 indices as the 2 indices left of the chosen index (extra prioritization of lesser chosen objectives/indices)
   def get3Indices(probs: NonEmptyList[Double]): (Int, Int, Int) =
-    if (probs.size == 3) // check for this before calculating probabilities (calling above funcs) etc. - save some comp time
+    if (probs.size == 3) // check for this before calculating probabilities (calling the above funcs) etc. - save some comp time
       (0, 1, 2) // 3 objectives -> use indices 0, 1, 2
-    else {
-
-      var pick = RVar.doubles(1).run(RNG.fromTime)._2.head // VAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-      val sortedProbs = probs.zipWithIndex.sortWith(_ > _) // probs wih indices sorted
+    else if (probs.size > 3) {
+      val pick = RVar.doubles(1).run(RNG.fromTime)._2.head // 0 < pick < 1
+      val sortedProbs = probs.zipWithIndex.sortWith(_ > _) // individual probs wih indices sorted
       val stick = sortedProbs.toList
         .scanLeft(0.0)((acc, el) => acc + el._1)
         .drop(1)
         .zip(sortedProbs.map(el => el._2).toList) // cumulative sorted probs with indices // drop is used to remove start of the 'stick' (0.0)
 
-      // i1
-      val indexIdentification1 = stick
+      // i123
+      val indexIdentification = stick
         .map(
           p =>
             if ((pick <= p._1) && (pick > 0) && (pick < 1))
@@ -51,52 +52,13 @@ object GetIndices {
             else
             None)
         .filter(_.isDefined)
-      val i1 =
-        if (indexIdentification1.empty) stick.reverse.head._2 else indexIdentification1.head.get
-
-      // i2
-      var i2 = -1 // VAR!!!!!!!!!!!!!!!!
-      var validIndexChosen = false // VAR!!!!!!!!!!!!!!!!!
-      while (!validIndexChosen) {
-        pick = RVar.doubles(1).run(RNG.fromTime)._2.head
-        val indexIdentification2 = stick
-          .map(
-            p =>
-              if ((pick <= p._1) && (pick > 0) && (pick < 1))
-                if (p._2 != i1)
-                  Some(p._2)
-                else
-                  None
-              else
-              None)
-          .filter(_.isDefined)
-        i2 =
-          if (indexIdentification2.empty) stick.reverse.head._2 else indexIdentification2.head.get
-        if (i2 != i1)
-          validIndexChosen = true
-      }
-
-      // i3
-      var i3 = -1 // VAR!!!!!!!!!!!!!!!!
-      validIndexChosen = false // VAR!!!!!!!!!!!!!!!!!
-      while (!validIndexChosen) {
-        pick = RVar.doubles(1).run(RNG.fromTime)._2.head
-        val indexIdentification3 = stick
-          .map(
-            p =>
-              if ((pick <= p._1) && (pick > 0) && (pick < 1))
-                if ((p._2 != i1) && (p._2 != i2))
-                  Some(p._2)
-                else
-                  None
-              else
-              None)
-          .filter(_.isDefined)
-        i3 =
-          if (indexIdentification3.empty) stick.reverse.head._2 else indexIdentification3.head.get
-        if ((i2 != i1) && (i3 != i1) && (i2 != i3))
-          validIndexChosen = true
-      }
-      (i1, i2, i3) // return 3 indices as tuple
+        if (indexIdentification.empty) throw new Exception("Error: pick did not satisfy any part of the stick.") // (stick.head._2, stick(1)._2, stick(2)._2) // should never be the case - just for safety
+        else if(indexIdentification.head.get == stick.reverse.head._2) (stick.head._2, stick(1)._2, stick.reverse.head._2) // if pick satisfies last part of stick
+        else if((indexIdentification.head.get == stick.head._2) || (indexIdentification.head.get == stick(1)._2) || (indexIdentification.head.get == stick(2)._2)) (stick.head._2, stick(1)._2, stick(2)._2) // if pick satisfies 1st, 2nd or 3rd part of stick from the front
+        else {
+          val i = stick.zipWithIndex.filter(_._1._2 == indexIdentification.head.get).head._2
+          (indexIdentification.head.get, stick(i - 1)._2, stick(i - 2)._2)
+        } // if pick satisfies part of stick in the middle (not 1st, 2nd, or 3rd, or last)
     }
+    else throw new Exception("Error: NonEmptyList of probabilities size < 3.")
 }
