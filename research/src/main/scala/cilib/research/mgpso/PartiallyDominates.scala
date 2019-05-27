1031561fa05e2521efc903b5c517530d7a2b5cfb
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-05-27 16:00:05 +0200

    Added extra parameters in order to save output files with a more descriptive name.

--- research/src/main/scala/cilib/research/mgpso/PartiallyDominates.scala
+++ research/src/main/scala/cilib/research/mgpso/PartiallyDominates.scala
@@ -4,15 +4,16 @@ import scalaz._
 import Scalaz._
 
 object PartiallyDominates { // ################################## CHANGED - no longer check for 3 obj since the way the random indices are astored caters for all number of obj already ########################
-	def apply(envX: Benchmark)(particleA: MGParticle, particleB: MGParticle): Boolean =
+
+	def apply(envX: Benchmark)(freqs_and_indices: PartialDominance) (particleA: MGParticle, particleB: MGParticle): Boolean =
 		if (particleA.pos.fitness == particleB.pos.fitness){
 			false
 		} else {
 			var check = false
 
 			// the 3 random indices readily available (even if only 3 obj)
-			val partialParticleA = List[Double](particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._1), particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._2), particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._3))
-			val partialParticleB = List[Double](particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._1), particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._2), particleA.pos.fitness.toList(envX.controlParameters.freqs.get._2._3))
+			val partialParticleA = List[Double](particleA.pos.fitness.toList(freqs_and_indices.i123._1), particleA.pos.fitness.toList(freqs_and_indices.i123._2), particleA.pos.fitness.toList(freqs_and_indices.i123._3))
+			val partialParticleB = List[Double](particleA.pos.fitness.toList(freqs_and_indices.i123._1), particleA.pos.fitness.toList(freqs_and_indices.i123._2), particleA.pos.fitness.toList(freqs_and_indices.i123._3))
 
 			partialParticleA.zip(partialParticleB).foldLeft(0)(
 				(a, c) => a +
