3de4c17c395ffefb3a5b0d6d1ecf7017252b7db9
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-05-28 11:13:11 +0200

    Added extra parameters in order to save output files with a more descriptive name.

--- research/src/main/scala/cilib/research/mgpso/Dominates.scala
+++ research/src/main/scala/cilib/research/mgpso/Dominates.scala
@@ -3,7 +3,7 @@ import cilib.research.core.Benchmark
 import scalaz.Scalaz._
 
 object Dominates {
-  def apply(envX: Benchmark)(particleA: MGParticle, particleB: MGParticle): Boolean =
+  def apply(envX: Benchmark)(particleA: MGParticle, particleB: MGParticle, notUsed: Option[PartialDominance]): Boolean =
     if (particleA.pos.fitness == particleB.pos.fitness) {
       false
     } else {
