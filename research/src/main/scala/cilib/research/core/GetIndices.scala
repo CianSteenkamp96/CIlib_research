7cb85672f6216b81cf4760de1678dcf0a723dd90
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-06-08 15:02:00 +0200

    Changes

---
+++ research/src/main/scala/cilib/research/core/GetIndices.scala
@@ -0,0 +1,63 @@
+// Cian Steenkamp
+
+package cilib.research.core
+import scalaz._
+import Scalaz._
+import cilib.{RNG, RVar}
+
+object GetIndices {
+
+// Assign 'fitness' vals based on frequency chosen
+  def fitnessFromFreq(freqs: NonEmptyList[Int]): NonEmptyList[Double] = {
+    val total = freqs.suml
+
+    if (total == 0 || freqs.toList.forall(_ == freqs.head)) // if all objectives have been chosen 0 times OR a equal number of times => dws equal prob
+      freqs.map(_ => 1.0 / freqs.size)
+    else
+      freqs.map(f => 1 - (f.toDouble / total)) // So that more popular objectives will be less likely to be picked and unpopular (low freq) will have a better chance of being chosen
+  }
+
+  // Assign a probability based on fitness vals
+  def probFromFitness(probs: NonEmptyList[Double]): NonEmptyList[Double] = {
+    val total = probs.foldLeft(0.0)(_ + _)
+
+    if (total == 0)
+      probs.map(_ => 1.0 / probs.size)
+    else
+      probs.map(f => f / total)
+  }
+
+  // Returns 3 indices/objectives to be considered for diminance relation (partial-dominance)
+  // This function prioritizes the indices/objectives with the largest probabilities (least chosen objectives) even more...
+  // Since, only one pick is made to save time - previous versions made 3 picks and had a lot of repititions to ensure 3 unique indices
+  // This implementation makes one pick, gets the index, and then chooses the other 2 indices as the 2 indices left of the chosen index (extra prioritization of lesser chosen objectives/indices)
+  def get3Indices(probs: NonEmptyList[Double]): (Int, Int, Int) =
+    if (probs.size == 3) // check for this before calculating probabilities (calling the above funcs) etc. - save some comp time
+      (0, 1, 2) // 3 objectives -> use indices 0, 1, 2
+    else if (probs.size > 3) {
+      val pick = RVar.doubles(1).run(RNG.fromTime)._2.head // 0 < pick < 1
+      val sortedProbs = probs.zipWithIndex.sortWith(_ > _) // individual probs wih indices sorted
+      val stick = sortedProbs.toList
+        .scanLeft(0.0)((acc, el) => acc + el._1)
+        .drop(1)
+        .zip(sortedProbs.map(el => el._2).toList) // cumulative sorted probs with indices // drop is used to remove start of the 'stick' (0.0)
+
+      // i123
+      val indexIdentification = stick
+        .map(
+          p =>
+            if ((pick <= p._1) && (pick > 0) && (pick < 1))
+              Some(p._2)
+            else
+            None)
+        .filter(_.isDefined)
+        if (indexIdentification.empty) throw new Exception("Error: pick did not satisfy any part of the stick.") // (stick.head._2, stick(1)._2, stick(2)._2) // should never be the case - just for safety
+        else if(indexIdentification.head.get == stick.reverse.head._2) (stick.head._2, stick(1)._2, stick.reverse.head._2) // if pick satisfies last part of stick
+        else if((indexIdentification.head.get == stick.head._2) || (indexIdentification.head.get == stick(1)._2) || (indexIdentification.head.get == stick(2)._2)) (stick.head._2, stick(1)._2, stick(2)._2) // if pick satisfies 1st, 2nd or 3rd part of stick from the front
+        else {
+          val i = stick.zipWithIndex.filter(_._1._2 == indexIdentification.head.get).head._2
+          (indexIdentification.head.get, stick(i - 1)._2, stick(i - 2)._2)
+        } // if pick satisfies part of stick in the middle (not 1st, 2nd, or 3rd, or last)
+    }
+    else throw new Exception("Error: NonEmptyList of probabilities size < 3.")
+}
--- research/src/main/scala/cilib/research/mgpso/MGPSO.scala
+++ research/src/main/scala/cilib/research/mgpso/MGPSO.scala
@@ -1,9 +1,11 @@
 package cilib.research.mgpso
 import cilib.research.MGArchive
 import cilib.research.core.{Benchmark, Position}
-import cilib.{Dist, RVar, Step, StepS}
-import scalaz.Scalaz._
+import cilib.{Dist, RNG, RVar, Step, StepS} // RNG new
 import scalaz._
+import Scalaz._
+import spire.implicits._ // NEW
+import spire.math.Interval // NEW
 
 object MGPSO {
 
@@ -41,13 +43,58 @@ object MGPSO {
   private def updatePBestBounds(envX: Benchmark)(p: MGParticle): StepS[Double, MGArchive, MGParticle] =
     if (p.pos.isInbounds) updatePBest(envX)(p) else MGStep.stepPure[Double, MGParticle](p)
 
+//  private def calcVelocity(particle: MGParticle,
+//                   social: Position,
+//                   cognitive: Position,
+//                   w: Double,
+//                   c1: Double,
+//                   c2: Double,
+//                   c3: Double) =
+//    MGStep.withArchive[Double, Position](archive => {
+//      if (archive.isEmpty) {
+//        Step.liftR(
+//          for {
+//            cog <- (cognitive - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//            soc <- (social - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//          } yield (w *: particle.velocity) + (c1 *: cog) + (c2 *: soc)
+//        )
+//      } else {
+//        Step.liftR(
+//          RVar
+//            .shuffle(archive.values.toNel.get)
+//            .flatMap(archiveList => {
+//              val tournament = archiveList.toList.take(3)
+//              val archiveGuide = CrowdingDistance.leastCrowded(tournament)
+//              for {
+//                cog <- (cognitive - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//                soc <- (social - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//                arc <- (archiveGuide.pos - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//                lambda <- particle.lambda.value
+//              } yield {
+//                (w *: particle.velocity) + (c1 *: cog) + (lambda *>: (c2 *: soc)) + (lambda.map(x =>
+//                  1 - x) *>: (c3 *: arc))
+//              }
+//            }))
+//      }
+//    })
+
+  /////////////////////////////////////////////////// NEW ///////////////////////////////////////////////////
+  // Calc velocity with random control params w, c1, c2, c3 which satisfies mgpso convergence criteria
   private def calcVelocity(particle: MGParticle,
-                   social: Position,
-                   cognitive: Position,
-                   w: Double,
-                   c1: Double,
-                   c2: Double,
-                   c3: Double) =
+                           social: Position,
+                           cognitive: Position) = {
+
+    val wc123 = satisfyStabilityCriteria(particle.lambda.value.eval(RNG.fromTime).head).eval(RNG.fromTime) // Correct way to access lambda double value? Also, why is lambda an RVar[NEL[Double]] and not just Rvar[Double] or just Double????????
+    val wc123Final = if(wc123.isDefined) (wc123.get._1, wc123.get._2, wc123.get._3, wc123.get._4) else (0.356, 1.222, 1.3, 1.517) // if finding suitable params took too long then return 'default' ctrl param vals.
+                                                                                                                                  // These were calculated as the average value per ctrl param over all 9 WFG problems taken from the optimal WFG 3 objective parameters.
+                                                                                                                                  // A.k.a => 3 objective WFG 1-9 w vals / 9, 3 objective WFG 1-9 c1 vals / 9, etc...
+                                                                                                                                  // These values satisfy the convergence condition for MGPSO (lambda vals were chosen (0, 1) and still satisfied the stability criteria regardless of lambda).
+
+    val w = wc123Final._1
+    val c1 = wc123Final._2
+    val c2 = wc123Final._3
+    val c3 = wc123Final._4
+
     MGStep.withArchive[Double, Position](archive => {
       if (archive.isEmpty) {
         Step.liftR(
@@ -75,6 +122,35 @@ object MGPSO {
             }))
       }
     })
+  }
+
+  /////////////////////////////////////////////////// NEW ///////////////////////////////////////////////////
+  // Thanks Gary for the help
+  def satisfyStabilityCriteria(lambda: Double): RVar[Option[(Double, Double, Double, Double)]] = {
+    def generator(counter: Int): RVar[Option[(Int, (Double, Double, Double, Double))]] =
+      for {
+        w  <- RVar.next[Double] // gen num between 0 and 1
+        c1 <- Dist.uniform(Interval.open(0.0, 2.0)) // gen num between 0 and 2
+        c2 <- Dist.uniform(Interval.open(0.0, 2.0)) // gen num between 0 and 2
+        c3 <- Dist.uniform(Interval.open(0.0, 2.0)) // gen num between 0 and 2
+      } yield Some((counter, (w, c1, c2, c3)))
+
+    val result: RVar[Option[(Int, (Double,Double,Double,Double))]] =
+      generator(0).flatMap {
+        case Some((counter, (w, c1, c2, c3))) =>
+          // MGPSO convergence/stability criteria
+          if(((c1 + (lambda * c2) + ((1 - lambda) * c3)) > 0) &&
+            ((c1 + (lambda * c2) + ((1 - lambda) * c3)) < ((4 * (1 - Math.pow(w, 2))) / (1 - w + ((Math.pow(c1, 2) + (Math.pow(lambda, 2) * Math.pow(c2, 2)) + ((Math.pow((1 - lambda), 2) * Math.pow(c3, 2)) * (1 + w))) / (3 * Math.pow((c1 + (lambda * c2) + ((1 - lambda) * c3)), 2))))))) {
+            RVar.pure(Some((counter, (w, c1, c2, c3))))
+          } else {
+            if (counter > 10) RVar.pure(None) else generator(counter+1) // If generating random values that satisfy the stability criteria takes to long then return None
+          }
+        case None =>
+          sys.error("impossible")
+      }
+//    result.map(option => option.map(Tuple2 => Tuple2._2))
+    result.map(_.map(_._2))
+  }
 
   private def updateVelocity(particle: MGParticle, v: Position) = MGStep.stepPure[Double, MGParticle] {
     particle.updateVelocity(v)
@@ -101,7 +177,8 @@ object MGPSO {
           _ <- insertIntoArchive(x)
           cog <- pbest(x)
           soc <- gbest(envX)(x, collection)
-          v <- calcVelocity(x, soc, cog, envX.controlParameters.w, envX.controlParameters.c1, envX.controlParameters.c2, envX.controlParameters.c3)
+//          v <- calcVelocity(x, soc, cog, envX.controlParameters.w, envX.controlParameters.c1, envX.controlParameters.c2, envX.controlParameters.c3)
+          v <- calcVelocity(x, soc, cog)
           p <- stdPosition(x, v)
           p2 <- multiEval(envX)(p)
           p3 <- updateVelocity(p2, v)
