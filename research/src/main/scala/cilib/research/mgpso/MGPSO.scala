faa90ccdefdc1525747d45d17be2ce539ed1a0a4
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-06-08 17:08:05 +0200

    Changes

--- research/src/main/scala/cilib/research/mgpso/MGPSO.scala
+++ research/src/main/scala/cilib/research/mgpso/MGPSO.scala
@@ -9,14 +9,15 @@ import spire.math.Interval // NEW
 
 object MGPSO {
 
-  private def multiEval(envX: Benchmark)(particle: MGParticle) = MGStep.stepPure[Double, MGParticle] {
-    val fitness = if (particle.pos.isInbounds) {
-      envX.f(particle.pos.pos)
-    } else {
-      particle.pos.fitness.map(_ => Double.MaxValue)
+  private def multiEval(envX: Benchmark)(particle: MGParticle) =
+    MGStep.stepPure[Double, MGParticle] {
+      val fitness = if (particle.pos.isInbounds) {
+        envX.f(particle.pos.pos)
+      } else {
+        particle.pos.fitness.map(_ => Double.MaxValue)
+      }
+      particle.updateFitness(fitness)
     }
-    particle.updateFitness(fitness)
-  }
 
   private def gbest(envX: Benchmark)(particle: MGParticle, collection: NonEmptyList[MGParticle]) =
     MGStep.stepPure[Double, Position] {
@@ -32,15 +33,17 @@ object MGPSO {
     particle.pb
   }
 
-  private def updatePBest(envX: Benchmark)(particle: MGParticle) = MGStep.stepPure[Double, MGParticle] {
-    if (envX.compare(particle.pos.fitness, particle.pb.fitness)) {
-      particle.updatePB
-    } else {
-      particle
+  private def updatePBest(envX: Benchmark)(particle: MGParticle) =
+    MGStep.stepPure[Double, MGParticle] {
+      if (envX.compare(particle.pos.fitness, particle.pb.fitness)) {
+        particle.updatePB
+      } else {
+        particle
+      }
     }
-  }
 
-  private def updatePBestBounds(envX: Benchmark)(p: MGParticle): StepS[Double, MGArchive, MGParticle] =
+  private def updatePBestBounds(envX: Benchmark)(
+      p: MGParticle): StepS[Double, MGArchive, MGParticle] =
     if (p.pos.isInbounds) updatePBest(envX)(p) else MGStep.stepPure[Double, MGParticle](p)
 
 //  private def calcVelocity(particle: MGParticle,
@@ -80,25 +83,64 @@ object MGPSO {
 
   /////////////////////////////////////////////////// NEW ///////////////////////////////////////////////////
   // Calc velocity with random control params w, c1, c2, c3 which satisfies mgpso convergence criteria
+//  private def calcVelocity(particle: MGParticle,
+//                           social: Position,
+//                           cognitive: Position) = {
+//
+//    val wc123 = satisfyStabilityCriteria(particle.lambda.value.eval(RNG.fromTime).head).eval(RNG.fromTime) // Correct way to access lambda double value? Also, why is lambda an RVar[NEL[Double]] and not just Rvar[Double] or just Double????????
+//    val wc123Final = if(wc123.isDefined) (wc123.get._1, wc123.get._2, wc123.get._3, wc123.get._4) else (0.356, 1.222, 1.3, 1.517) // if finding suitable params took too long then return 'default' ctrl param vals.
+//                                                                                                                                  // These were calculated as the average value per ctrl param over all 9 WFG problems taken from the optimal WFG 3 objective parameters.
+//                                                                                                                                  // A.k.a => 3 objective WFG 1-9 w vals / 9, 3 objective WFG 1-9 c1 vals / 9, etc...
+//                                                                                                                                  // These values satisfy the convergence condition for MGPSO (lambda vals were chosen (0, 1) and still satisfied the stability criteria regardless of lambda).
+////    particle.lambda.value.map()
+//    val w = wc123Final._1
+//    val c1 = wc123Final._2
+//    val c2 = wc123Final._3
+//    val c3 = wc123Final._4
+//
+//    MGStep.withArchive[Double, Position](archive => {
+//      if (archive.isEmpty) {
+//        Step.liftR(
+//          for {
+//            cog <- (cognitive - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//            soc <- (social - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//          } yield (w *: particle.velocity) + (c1 *: cog) + (c2 *: soc)
+//        )
+//      } else {
+//        Step.liftR(
+//          RVar
+//            .shuffle(archive.values.toNel.get)
+//            .flatMap(archiveList => {
+//              val tournament = archiveList.toList.take(3)
+//              val archiveGuide = CrowdingDistance.leastCrowded(tournament)
+//              for {
+//                cog <- (cognitive - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//                soc <- (social - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//                arc <- (archiveGuide.pos - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
+//                lambda <- particle.lambda.value
+//              } yield {
+//                (w *: particle.velocity) + (c1 *: cog) + (lambda *>: (c2 *: soc)) + (lambda.map(x =>
+//                  1 - x) *>: (c3 *: arc))
+//              }
+//            }))
+//      }
+//    })
+//  }
   private def calcVelocity(particle: MGParticle,
                            social: Position,
-                           cognitive: Position) = {
-
-    val wc123 = satisfyStabilityCriteria(particle.lambda.value.eval(RNG.fromTime).head).eval(RNG.fromTime) // Correct way to access lambda double value? Also, why is lambda an RVar[NEL[Double]] and not just Rvar[Double] or just Double????????
-    val wc123Final = if(wc123.isDefined) (wc123.get._1, wc123.get._2, wc123.get._3, wc123.get._4) else (0.356, 1.222, 1.3, 1.517) // if finding suitable params took too long then return 'default' ctrl param vals.
-                                                                                                                                  // These were calculated as the average value per ctrl param over all 9 WFG problems taken from the optimal WFG 3 objective parameters.
-                                                                                                                                  // A.k.a => 3 objective WFG 1-9 w vals / 9, 3 objective WFG 1-9 c1 vals / 9, etc...
-                                                                                                                                  // These values satisfy the convergence condition for MGPSO (lambda vals were chosen (0, 1) and still satisfied the stability criteria regardless of lambda).
-
-    val w = wc123Final._1
-    val c1 = wc123Final._2
-    val c2 = wc123Final._3
-    val c3 = wc123Final._4
-
-    MGStep.withArchive[Double, Position](archive => {
+                           cognitive: Position): StepS[Double, MGArchive, Position] =
+    MGStep.withArchive[Double, Position](archive => { // See Gitter chat with Kyle Erwin explaining this code block ...
       if (archive.isEmpty) {
-        Step.liftR(
+        Step.liftR( 								 // ... and this code block.
           for {
+            wc123 <- particle.lambda.value.map(list => satisfyStabilityCriteria(list.head))
+            wc123Final = wc123
+              .eval(RNG.fromTime)
+              .getOrElse((0.356, 1.222, 1.3, 1.517)) // Still eval here?
+            w = wc123Final._1
+            c1 = wc123Final._2
+            c2 = wc123Final._3
+            c3 = wc123Final._4
             cog <- (cognitive - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
             soc <- (social - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
           } yield (w *: particle.velocity) + (c1 *: cog) + (c2 *: soc)
@@ -111,6 +153,14 @@ object MGPSO {
               val tournament = archiveList.toList.take(3)
               val archiveGuide = CrowdingDistance.leastCrowded(tournament)
               for {
+                wc123 <- particle.lambda.value.map(list => satisfyStabilityCriteria(list.head))
+                wc123Final = wc123
+                  .eval(RNG.fromTime)
+                  .getOrElse((0.356, 1.222, 1.3, 1.517)) // Still eval here?
+                w = wc123Final._1
+                c1 = wc123Final._2
+                c2 = wc123Final._3
+                c3 = wc123Final._4
                 cog <- (cognitive - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
                 soc <- (social - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
                 arc <- (archiveGuide.pos - particle.pos).traverse(x => Dist.stdUniform.map(_ * x))
@@ -122,28 +172,29 @@ object MGPSO {
             }))
       }
     })
-  }
-
   /////////////////////////////////////////////////// NEW ///////////////////////////////////////////////////
   // Thanks Gary for the help
   def satisfyStabilityCriteria(lambda: Double): RVar[Option[(Double, Double, Double, Double)]] = {
     def generator(counter: Int): RVar[Option[(Int, (Double, Double, Double, Double))]] =
       for {
-        w  <- RVar.next[Double] // gen num between 0 and 1
+        w <- RVar.next[Double] // gen num between 0 and 1
         c1 <- Dist.uniform(Interval.open(0.0, 2.0)) // gen num between 0 and 2
         c2 <- Dist.uniform(Interval.open(0.0, 2.0)) // gen num between 0 and 2
         c3 <- Dist.uniform(Interval.open(0.0, 2.0)) // gen num between 0 and 2
       } yield Some((counter, (w, c1, c2, c3)))
 
-    val result: RVar[Option[(Int, (Double,Double,Double,Double))]] =
+    val result: RVar[Option[(Int, (Double, Double, Double, Double))]] =
       generator(0).flatMap {
         case Some((counter, (w, c1, c2, c3))) =>
           // MGPSO convergence/stability criteria
-          if(((c1 + (lambda * c2) + ((1 - lambda) * c3)) > 0) &&
-            ((c1 + (lambda * c2) + ((1 - lambda) * c3)) < ((4 * (1 - Math.pow(w, 2))) / (1 - w + ((Math.pow(c1, 2) + (Math.pow(lambda, 2) * Math.pow(c2, 2)) + ((Math.pow((1 - lambda), 2) * Math.pow(c3, 2)) * (1 + w))) / (3 * Math.pow((c1 + (lambda * c2) + ((1 - lambda) * c3)), 2))))))) {
+          if (((c1 + (lambda * c2) + ((1 - lambda) * c3)) > 0) &&
+              ((c1 + (lambda * c2) + ((1 - lambda) * c3)) < ((4 * (1 - Math.pow(w, 2))) / (1 - w + ((Math
+                .pow(c1, 2) + (Math.pow(lambda, 2) * Math.pow(c2, 2)) + ((Math.pow((1 - lambda), 2) * Math
+                .pow(c3, 2)) * (1 + w))) / (3 * Math.pow((c1 + (lambda * c2) + ((1 - lambda) * c3)),
+                                                         2))))))) {
             RVar.pure(Some((counter, (w, c1, c2, c3))))
           } else {
-            if (counter > 10) RVar.pure(None) else generator(counter+1) // If generating random values that satisfy the stability criteria takes to long then return None
+            if (counter > 10) RVar.pure(None) else generator(counter + 1) // If generating random values that satisfy the stability criteria takes to long then return None
           }
         case None =>
           sys.error("impossible")
@@ -152,9 +203,10 @@ object MGPSO {
     result.map(_.map(_._2))
   }
 
-  private def updateVelocity(particle: MGParticle, v: Position) = MGStep.stepPure[Double, MGParticle] {
-    particle.updateVelocity(v)
-  }
+  private def updateVelocity(particle: MGParticle, v: Position) =
+    MGStep.stepPure[Double, MGParticle] {
+      particle.updateVelocity(v)
+    }
 
   private def updateLambda(particle: MGParticle) = MGStep.stepPure[Double, MGParticle] {
     particle.updateLambda
@@ -165,8 +217,7 @@ object MGPSO {
   }
 
   private def insertIntoArchive(particle: MGParticle) =
-    MGStep.modifyArchive { archive =>
-      archive.insert(particle)
+    MGStep.modifyArchive { archive => archive.insert(particle)
     }
 
   def mgpso(envX: Benchmark)
