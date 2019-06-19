392053a27be782b8cf44d7d3ed5947e29fa16194
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-06-19 13:11:57 +0200

    Changes

---
+++ research/src/main/scala/cilib/research/Hello.scala
@@ -0,0 +1,15 @@
+package cilib.research
+
+// object Hello extends Greeting with App {
+//   println(greeting)
+// }
+
+// trait Greeting {
+//   lazy val greeting: String = "hello"
+// }
+
+object Hello {
+  def main(args:Array[String]) = {
+    println("Hello World!!")
+  }
+}
