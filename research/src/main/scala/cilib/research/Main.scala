57050281071269c66c8f7a6faf0fda6ba0963440
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-07-23 15:25:43 +0200

    Changes

--- research/src/main/scala/cilib/research/Main.scala
+++ research/src/main/scala/cilib/research/Main.scala
@@ -12,11 +12,24 @@ import scalaz.effect.{IO, SafeApp}
 object Main extends SafeApp {
 
   override def run(args: ImmutableArray[String]): IO[Unit] = {
-    if(args.length != 4) throw new Exception("Number of CL params needs to be 4 separated by spaces including: algoName (MGPSO or PMGPSO) numObjs (3, 5, 8, 10, or 15) numDims (30, 100, 500, or 1000) problemSuite (WFG or DTLZ).\n")
+    if(args.length != 4 && args.length != 5) throw new Exception("Number of CL params needs to be at least 4 separated by spaces including: algoName (MGPSO or PMGPSO) numObjs (3, 5, 8, 10, or 15) numDims (30, 100, 500, or 1000) problemSuite (WFG or DTLZ) problemNum (Note this parameter is optional - if left unspecified the entire problem suite will be included. For WFG: 1, 2, ..., or 9; For DTLZ: 1, 2, ..., or 7).\n")
     else if((args(0) != "MGPSO" && args(0) != "PMGPSO") ||
       (args(1).toInt != 3 && args(1).toInt != 5 && args(1).toInt != 8 && args(1).toInt != 10 && args(1).toInt != 15) ||
       (args(2).toInt != 30 && args(2).toInt != 100 && args(2).toInt != 500 && args(2).toInt != 1000) ||
       (args(3) != "WFG" && args(3) != "DTLZ")) throw new Exception("CL params needs to be: algoName (MGPSO or PMGPSO) numObjs (3, 5, 8, 10, or 15) numDims (30, 100, 500, or 1000) problemSuite (WFG or DTLZ).\n")
+
+    if(args.length == 5)
+      if(args(3) == "WFG")
+        if(args(4).toInt != 1 && args(4).toInt != 2 && args(4).toInt != 3 && args(4).toInt != 4 && args(4).toInt != 5 && args(4).toInt != 6 && args(4).toInt != 7 && args(4).toInt != 8 && args(4).toInt != 9)
+          throw new Exception("The fifth CL parameter (the problem number), for WFG, should be: 1, 2, ..., or 9.\n")
+      else if(args(3) == "DTLZ")
+        if(args(4).toInt != 1 && args(4).toInt != 2 && args(4).toInt != 3 && args(4).toInt != 4 && args(4).toInt != 5 && args(4).toInt != 6 && args(4).toInt != 7)
+          throw new Exception("The fifth CL parameter (the problem number), for WFG, should be: 1, 2, ..., or 7.\n")
+      else if(args(3) == "ZDT")
+        if(args(4).toInt != 1 && args(4).toInt != 2 && args(4).toInt != 3 && args(4).toInt != 4 && args(4).toInt != 6)
+          throw new Exception("The fifth CL parameter (the problem number), for WFG, should be: 1, 2, 3, 4, or 6.\n")
+      else throw new Exception("Test suite should be one of the following: \"WFG\", \"DTLZ\", or \"ZDT\".\n")
+
 
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CONFIG CHANGES HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     // This (or at least some of it) will be passed in through the CLI as CL params. Some fixed for convenience of research. But the code can easily be changed to set everything through the CL.
@@ -25,8 +38,8 @@ object Main extends SafeApp {
       : String = args(0) // algorithm name: 'MGPSO' or 'PMGPSO' - still to possibly implement KnMGPSO, KnPMGPSO
                           // see note in office cupboard explaining different Knea Point MGPSO options/configs/combos of insert policies and archive guide selection
     val lambdaStrategy: String = "R" // lambda strategy: 'R', 'STD', 'LI', 'LD', 'RI', or 'RIJ'
-    val iterations: Int = 2000 // #iterations per independent sample/run
-    val independentRuns: Int = 30 // #independent samples: 30
+    val iterations: Int = 20 // #iterations per independent sample/run
+    val independentRuns: Int = 3 // #independent samples: 30
     val numObj: Int = args(1).toInt // #obj: 3, 5, 8, 10, or 15
     val numDecisionVariables: Int = args(2).toInt // #dimensions in the decision space: 30, 100, 500, or 1000
     val subswarms3obj: NonEmptyList[Int] = NonEmptyList(51, 51, 51) // 153
@@ -34,7 +47,7 @@ object Main extends SafeApp {
     val subswarms8obj: NonEmptyList[Int] = NonEmptyList(19, 19, 19, 19, 19, 19, 19, 20) // 156
     val subswarms10obj: NonEmptyList[Int] = NonEmptyList(11, 11, 11, 11, 11, 11, 11, 11, 11, 11) // 110
     val subswarms15obj: NonEmptyList[Int] = NonEmptyList(9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9) // 135
-
+    val problemNum: Int = if(args.length == 5) args(4).toInt else -1
     // NonEmptyList of sub swarm divisions;
     // number of objectives equal to the size of the swarms NonEmptyList,
     // that is, each element in the NEL represents a sub swarm's size
@@ -52,14 +65,14 @@ object Main extends SafeApp {
     // Unnecessary to checks here since done above
     val benchmarkSuite =
       if (benchmarkSuiteName == "WFG")
-        BenchmarkSuite.wfgObj(numObjectives, numDecisionVariables, swarms)
+        BenchmarkSuite.wfgObj(numObjectives, numDecisionVariables, swarms, problemNum)
       else if (benchmarkSuiteName == "DTLZ")
-        BenchmarkSuite.dtlzObj(numObjectives, numDecisionVariables, swarms)
+        BenchmarkSuite.dtlzObj(numObjectives, numDecisionVariables, swarms, problemNum)
       else if (benchmarkSuiteName == "ZDT")
         if (numObjectives != 2)
           throw new Exception("ZDT is bi-objective.\n")
         else
-          BenchmarkSuite.zdtObj(swarms = swarms) // ZDT is only bi-objective and has its own specific number of decision variables
+          BenchmarkSuite.zdtObj(swarms, problemNum) // ZDT is only bi-objective and has its own specific number of decision variables
       else
         throw new Exception(
           "Test suite should be one of the following: \"WFG\", \"DTLZ\", or \"ZDT\".\n")
--- research/src/main/scala/cilib/research/core/BenchmarkSuite.scala
+++ research/src/main/scala/cilib/research/core/BenchmarkSuite.scala
@@ -17,66 +17,148 @@ object BenchmarkSuite {
 
   def wfgObj(numObjectives: Int,
              numDecisionVariables: Int,
-             swarms: NonEmptyList[Int]): BenchmarkSuite = {
+             swarms: NonEmptyList[Int],
+             problemNum: Int): BenchmarkSuite = {
     val wfgBounds =
       (0 until numDecisionVariables).toList.toNel.get.map(x => Interval(0.0, 2.0 * x + 1.0))
     BenchmarkSuite(
       "wfg" + numObjectives + "obj",
-      NonEmptyList(
-        Benchmark("WFG1", WFG.WFG1(numObjectives), wfgBounds, ControlParameters(swarms)),
-        Benchmark("WFG2", WFG.WFG2(numObjectives), wfgBounds, ControlParameters(swarms)),
-        Benchmark("WFG3", WFG.WFG3(numObjectives), wfgBounds, ControlParameters(swarms)),
-        Benchmark("WFG4", WFG.WFG4(numObjectives), wfgBounds, ControlParameters(swarms)),
-        Benchmark("WFG5", WFG.WFG5(numObjectives), wfgBounds, ControlParameters(swarms)),
-        Benchmark("WFG6", WFG.WFG6(numObjectives), wfgBounds, ControlParameters(swarms)),
-        Benchmark("WFG7", WFG.WFG7(numObjectives), wfgBounds, ControlParameters(swarms)),
-        Benchmark("WFG8", WFG.WFG8(numObjectives), wfgBounds, ControlParameters(swarms)),
-        Benchmark("WFG9", WFG.WFG9(numObjectives), wfgBounds, ControlParameters(swarms))
-      )
+      if(problemNum == 1)
+        NonEmptyList(
+          Benchmark("WFG1", WFG.WFG1(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
+      else if(problemNum == 2)
+        NonEmptyList(
+          Benchmark("WFG2", WFG.WFG2(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
+      else if(problemNum == 3)
+        NonEmptyList(
+          Benchmark("WFG3", WFG.WFG3(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
+      else if(problemNum == 4)
+        NonEmptyList(
+          Benchmark("WFG4", WFG.WFG4(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
+      else if(problemNum == 5)
+        NonEmptyList(
+          Benchmark("WFG5", WFG.WFG5(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
+      else if(problemNum == 6)
+        NonEmptyList(
+          Benchmark("WFG6", WFG.WFG6(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
+      else if(problemNum == 7)
+        NonEmptyList(
+          Benchmark("WFG7", WFG.WFG7(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
+      else if(problemNum == 8)
+        NonEmptyList(
+          Benchmark("WFG8", WFG.WFG8(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
+      else if(problemNum == 9)
+        NonEmptyList(
+          Benchmark("WFG9", WFG.WFG9(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
+      else  // poblemNum == -1
+        NonEmptyList(
+          Benchmark("WFG1", WFG.WFG1(numObjectives), wfgBounds, ControlParameters(swarms)),
+          Benchmark("WFG2", WFG.WFG2(numObjectives), wfgBounds, ControlParameters(swarms)),
+          Benchmark("WFG3", WFG.WFG3(numObjectives), wfgBounds, ControlParameters(swarms)),
+          Benchmark("WFG4", WFG.WFG4(numObjectives), wfgBounds, ControlParameters(swarms)),
+          Benchmark("WFG5", WFG.WFG5(numObjectives), wfgBounds, ControlParameters(swarms)),
+          Benchmark("WFG6", WFG.WFG6(numObjectives), wfgBounds, ControlParameters(swarms)),
+          Benchmark("WFG7", WFG.WFG7(numObjectives), wfgBounds, ControlParameters(swarms)),
+          Benchmark("WFG8", WFG.WFG8(numObjectives), wfgBounds, ControlParameters(swarms)),
+          Benchmark("WFG9", WFG.WFG9(numObjectives), wfgBounds, ControlParameters(swarms))
+        )
     )
   }
 
   def dtlzObj(numObjectives: Int,
               numDecisionVariables: Int,
-              swarms: NonEmptyList[Int]): BenchmarkSuite = {
+              swarms: NonEmptyList[Int],
+              problemNum: Int): BenchmarkSuite = {
     val dtlzBounds =
       (0 until numDecisionVariables).toList.toNel.get.map(x => Interval(0.0, 1.0))
     BenchmarkSuite(
       "dtlz" + numObjectives + "obj",
-      NonEmptyList(
-        Benchmark("DTLZ1",
-                  DTLZ.DTLZ1F(numObjectives, numDecisionVariables),
-                  dtlzBounds,
-                  ControlParameters(swarms)),
-        Benchmark("DTLZ2",
-                  DTLZ.DTLZ2F(numObjectives, numDecisionVariables),
-                  dtlzBounds,
-                  ControlParameters(swarms)),
-        Benchmark("DTLZ3",
-                  DTLZ.DTLZ3F(numObjectives, numDecisionVariables),
-                  dtlzBounds,
-                  ControlParameters(swarms)),
-        Benchmark("DTLZ4",
-                  DTLZ.DTLZ4F(numObjectives, numDecisionVariables),
-                  dtlzBounds,
-                  ControlParameters(swarms)),
-        Benchmark("DTLZ5",
-                  DTLZ.DTLZ5F(numObjectives, numDecisionVariables),
-                  dtlzBounds,
-                  ControlParameters(swarms)),
-        Benchmark("DTLZ6",
-                  DTLZ.DTLZ6F(numObjectives, numDecisionVariables),
-                  dtlzBounds,
-                  ControlParameters(swarms)),
-        Benchmark("DTLZ7",
-                  DTLZ.DTLZ7F(numObjectives, numDecisionVariables),
-                  dtlzBounds,
-                  ControlParameters(swarms)),
-      )
+      if(problemNum == 1)
+        NonEmptyList(
+          Benchmark("DTLZ1",
+            DTLZ.DTLZ1F(numObjectives, numDecisionVariables),
+            dtlzBounds,
+            ControlParameters(swarms)))
+      else if(problemNum == 2)
+        NonEmptyList(
+          Benchmark("DTLZ2",
+            DTLZ.DTLZ2F(numObjectives, numDecisionVariables),
+            dtlzBounds,
+            ControlParameters(swarms)))
+      else if(problemNum == 3)
+        NonEmptyList(
+          Benchmark("DTLZ3",
+            DTLZ.DTLZ3F(numObjectives, numDecisionVariables),
+            dtlzBounds,
+            ControlParameters(swarms)))
+      else if(problemNum == 4)
+        NonEmptyList(
+          Benchmark("DTLZ4",
+            DTLZ.DTLZ4F(numObjectives, numDecisionVariables),
+            dtlzBounds,
+            ControlParameters(swarms)))
+      else if(problemNum == 5)
+        NonEmptyList(
+          Benchmark("DTLZ5",
+            DTLZ.DTLZ5F(numObjectives, numDecisionVariables),
+            dtlzBounds,
+            ControlParameters(swarms)))
+      else if(problemNum == 6)
+        NonEmptyList(
+          Benchmark("DTLZ6",
+            DTLZ.DTLZ6F(numObjectives, numDecisionVariables),
+            dtlzBounds,
+            ControlParameters(swarms)))
+      else if(problemNum == 7)
+        NonEmptyList(
+          Benchmark("DTLZ7",
+            DTLZ.DTLZ7F(numObjectives, numDecisionVariables),
+            dtlzBounds,
+            ControlParameters(swarms)))
+      else // poblemNum == -1
+        NonEmptyList(
+          Benchmark("DTLZ1",
+                    DTLZ.DTLZ1F(numObjectives, numDecisionVariables),
+                    dtlzBounds,
+                    ControlParameters(swarms)),
+          Benchmark("DTLZ2",
+                    DTLZ.DTLZ2F(numObjectives, numDecisionVariables),
+                    dtlzBounds,
+                    ControlParameters(swarms)),
+          Benchmark("DTLZ3",
+                    DTLZ.DTLZ3F(numObjectives, numDecisionVariables),
+                    dtlzBounds,
+                    ControlParameters(swarms)),
+          Benchmark("DTLZ4",
+                    DTLZ.DTLZ4F(numObjectives, numDecisionVariables),
+                    dtlzBounds,
+                    ControlParameters(swarms)),
+          Benchmark("DTLZ5",
+                    DTLZ.DTLZ5F(numObjectives, numDecisionVariables),
+                    dtlzBounds,
+                    ControlParameters(swarms)),
+          Benchmark("DTLZ6",
+                    DTLZ.DTLZ6F(numObjectives, numDecisionVariables),
+                    dtlzBounds,
+                    ControlParameters(swarms)),
+          Benchmark("DTLZ7",
+                    DTLZ.DTLZ7F(numObjectives, numDecisionVariables),
+                    dtlzBounds,
+                    ControlParameters(swarms)),
+        )
     )
   }
 
-  def zdtObj(swarms: NonEmptyList[Int]): BenchmarkSuite = {
+  def zdtObj(swarms: NonEmptyList[Int], problemNum: Int): BenchmarkSuite = {
     val zdt1Bounds = Interval(0.0, 1.0) ^ 30
     val zdt2Bounds = Interval(0.0, 1.0) ^ 30
     val zdt3Bounds = Interval(0.0, 1.0) ^ 30
@@ -85,13 +167,24 @@ object BenchmarkSuite {
 
     BenchmarkSuite(
       "ZDT",
-      NonEmptyList(
-        Benchmark("ZDT1", ZDT.ZDT1F, zdt1Bounds, ControlParameters(swarms)),
-        Benchmark("ZDT2", ZDT.ZDT2F, zdt2Bounds, ControlParameters(swarms)),
-        Benchmark("ZDT3", ZDT.ZDT3F, zdt3Bounds, ControlParameters(swarms)),
-        Benchmark("ZDT4", ZDT.ZDT4F, zdt4Bounds, ControlParameters(swarms)),
-        Benchmark("ZDT6", ZDT.ZDT6F, zdt6Bounds, ControlParameters(swarms))
-      )
+      if(problemNum == 1)
+        NonEmptyList(Benchmark("ZDT1", ZDT.ZDT1F, zdt1Bounds, ControlParameters(swarms)))
+      else if(problemNum == 2)
+        NonEmptyList(Benchmark("ZDT2", ZDT.ZDT2F, zdt2Bounds, ControlParameters(swarms)))
+      else if(problemNum == 3)
+        NonEmptyList(Benchmark("ZDT3", ZDT.ZDT3F, zdt3Bounds, ControlParameters(swarms)))
+      else if(problemNum == 4)
+        NonEmptyList(Benchmark("ZDT4", ZDT.ZDT4F, zdt4Bounds, ControlParameters(swarms)))
+      else if(problemNum == 6)
+        NonEmptyList(Benchmark("ZDT6", ZDT.ZDT6F, zdt6Bounds, ControlParameters(swarms)))
+      else // poblemNum == -1
+        NonEmptyList(
+          Benchmark("ZDT1", ZDT.ZDT1F, zdt1Bounds, ControlParameters(swarms)),
+          Benchmark("ZDT2", ZDT.ZDT2F, zdt2Bounds, ControlParameters(swarms)),
+          Benchmark("ZDT3", ZDT.ZDT3F, zdt3Bounds, ControlParameters(swarms)),
+          Benchmark("ZDT4", ZDT.ZDT4F, zdt4Bounds, ControlParameters(swarms)),
+          Benchmark("ZDT6", ZDT.ZDT6F, zdt6Bounds, ControlParameters(swarms))
+        )
     )
   }
 }
